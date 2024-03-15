2b3b37f6895858bd91d399e8512b7ced1c2ddea0
lorenzo spizzuoco <=>
2024-03-15 18:07:07 +0100

    first version poor performance

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-import moviepy
+from moviepy.editor import concatenate_videoclips, VideoFileClip
 
 if __name__ == "__main__":
     import argparse
+
+    # arg parser
     parser = argparse.ArgumentParser(
             description="video concatenation tool, concatentate every mp4 file in a folder")
 
@@ -11,7 +13,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     path = args.path
+    print(path)
     output = args.output
-    for filepath in os.listdir(path):
-        print(filepath)
+    if path == None:
+        print("input path must be not None")
+    else:
+        if output == None:
+            output = path + "output.mp4"
 
+        videos = [VideoFileClip(path + "/"  + video) for video in os.listdir(path) if os.path.splitext(video)[1] == ".mp4"]
+        video = concatenate_videoclips(videos, method="compose")
+        video.write_videofile(output, threads = 8, fps = 24)
+        
+        print(videos)
+        
